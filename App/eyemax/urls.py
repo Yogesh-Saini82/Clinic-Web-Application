15880d09785652fc13e9cbc5b4f3fb01f52122e5
Yogeshsaini <yogesh.saini@intrics.in>
2024-11-18 12:29:01 +0530

    update code

--- App/eyemax/urls.py
+++ App/eyemax/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from eyemax import views
-from .views import book_appointment, appointment_list
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/', book_appointment, name='book_appointment'),
-    path('appointments/', appointment_list, name='appointment_list'),
 ]
--- App/eyemax/views.py
+++ App/eyemax/views.py
@@ -1,21 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import Appointment, Service
-from .forms import AppointmentForm
+from django.shortcuts import render
+
 
 def index(request):
     return render(request, 'Homepage.html')
 
-
-def book_appointment(request):
-    if request.method == 'POST':
-        form = AppointmentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('appointment_success')
-    else:
-        form = AppointmentForm()
-    return render(request, 'book_appointment.html', {'form': form})
-
-def appointment_list(request):
-    appointments = Appointment.objects.filter(user=request.user)
-    return render(request, 'appointment_list.html', {'appointments': appointments})
